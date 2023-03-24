b47b5251ea530adb0f6b2ddc92df5b4b6768734d
Yui <dev@sleepyyui.com>
2023-03-24 15:53:51 +0100

    eh

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+;
+fibonacci.c,a\9\a9b3911ffd3f456f1e3d9f3cb0a81f84d80f2e58
---
+++ fibonaccimem.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+void fib(unsigned long n, mpz_t *memo[]) {
+    if (mpz_cmp_ui(*memo[n % 2], 0) != 0) {
+        return;
+    }
+    if (n <= 2) {
+        mpz_set_ui(*memo[n % 2], 1);
+        return;
+    }
+    unsigned long i;
+    for (i = 2; i <= n; i++) {
+        mpz_add(*memo[i % 2], *memo[(i - 1) % 2], *memo[(i - 2) % 2]);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // set n to the args provided
+    unsigned long n;
+    if (argc == 2) {
+        n = strtoul(argv[1], NULL, 10);
+    } else {
+        fprintf(stderr, "usage: fib <n>\n");
+        exit(EXIT_FAILURE);
+    }
+    mpz_t *memo[2];
+    memo[0] = calloc(1, sizeof(mpz_t));
+    memo[1] = calloc(1, sizeof(mpz_t));
+    mpz_init(memo[0][0]);
+    mpz_init(memo[1][0]);
+    fib(n, memo);
+    printf("done\nresult:\n");
+    //mpz_out_str(stdout, 10, memo[n % 2][0]);
+    printf("\n");
+    mpz_clear(memo[0][0]);
+    mpz_clear(memo[1][0]);
+    free(memo[0]);
+    free(memo[1]);
+    return EXIT_SUCCESS;
+}
